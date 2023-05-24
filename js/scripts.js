ef0e23680b05196a8b59d1340de0156b3f54708d
Jithin K Jose <jithin.jude68@gmail.com>
2023-05-24 23:52:17 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -175,17 +175,25 @@
 		 
         /* Latest updates slider */
 	    
-document.addEventListener("DOMContentLoaded", function() {
-  var slides = document.querySelectorAll(".slide");
-  var currentSlide = 0;
-  var slideInterval = setInterval(nextSlide, 2000); // Adjust the interval (in milliseconds) as needed
+let slideIndex = 0;
+showSlides();
 
-  function nextSlide() {
-    slides[currentSlide].classList.remove("active");
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add("active");
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-});    
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 	   
 		 
         /* Testimonial Carousel/Slider */
