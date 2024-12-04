2f97c116c43f67e535a8a7ad0d82f90a48d2f7e1
Jithin Jude <jithin.jude68@gmail.com>
2024-12-04 13:22:17 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -117,7 +117,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["Android/Flutter Developer.", "I Make Apps."],    /* You can change the home section typing text from
+            strings: ["Independent Application Developer.", "I Make Apps."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 50
           });
